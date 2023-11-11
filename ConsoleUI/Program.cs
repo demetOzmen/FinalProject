c2853524579719bc8684dbb8ac6e0edacf663ee0
Demet AKDOĞAN <demet.akd@hotmail.com>
2023-11-11 12:15:16 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        foreach (var product in productManager.GetAllByCategoryId(2))
+        {
+            Console.WriteLine(product.ProductName);
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje claslarını bağlamak
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=Northwind;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;",
+    options => options.EnableRetryOnFailure(maxRetryCount: 2, maxRetryDelay: TimeSpan.FromSeconds(5), errorNumbersToAdd: null)
+           );
+        }
+        public DbSet <Product> Products{ get; set; }
+        public DbSet <Category> Categories{ get; set; }
+        public DbSet <Customer> Customers{ get; set; }
+    }
+}
