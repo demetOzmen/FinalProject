8dbab7e49610fea40d18a55460552a4f6498c8d1
Demet AKDOĞAN <demet.akd@hotmail.com>
2023-11-18 00:24:45 +0300

    CampCourse11Finished

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,7 @@ namespace Entities.Concrete
 {
     public class Product:IEntity
     {
+    
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
